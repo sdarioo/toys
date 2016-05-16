4698b9dee2f7adfc169d35035b9c927b8e348352
dario <dario_79@poczta.fm>
2016-05-16 23:24:25 +0200

    Added spring folder

---
+++ spring/web-spring-annot/src/main/java/config/ServletContainerInitializer.java
@@ -0,0 +1,47 @@
+package config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletContainerInitializer
+	extends AbstractAnnotationConfigDispatcherServletInitializer
+{
+
+
+    @Override
+    protected String getServletName()
+    {
+        return "spring-mvc";
+    }
+	
+    @Override
+    protected Class<?>[] getRootConfigClasses()
+    {
+        return new Class[]
+        {
+            ApplicationContext.class
+        };
+    }
+
+    /**
+     * SpringMVC configuration.
+     */
+    @Override
+    protected Class<?>[] getServletConfigClasses()
+    {
+        return new Class[]
+        {
+            SpringMVCApplicationContext.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings()
+    {
+        return new String[]
+        {
+            "/"
+        };
+    }
+	
+
+}
